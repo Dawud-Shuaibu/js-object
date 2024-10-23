514591ed7b2ad54ef56f472ec234d12f5b34c6dc
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2024-10-23 14:07:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// JavaScript Onject
+
+let person = {
+    name: 'John',
+    lastName: 'Smith'
+};
+
+// Using Dot Notation
+person.name = 'Dawud'; // Changing a property value
+person.age = 20; // Adding a peoperty
+
+// Using Bracket Notation
+person['lastName'] = 'Shuaibu'; // Changing a property value
+person['level'] = 100; // Adding a peoperty
+
+// Logging the object property on the console
+console.log(person);
+
+// Dot Notation
+console.log(person.name); 
+console.log(person.lastName);
+
+// Bracket Notation
+console.log(person['age']);
+console.log(person['level']);
